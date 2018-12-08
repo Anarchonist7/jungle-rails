17083658f6f6a5f6508be0b74df9c4c4d3f39bf6
Dawson Ley <anarchonist@protonmail.com>
2018-12-08 02:20:32 +0000

    logged in users can delete their own posts

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,26 @@
 class ReviewsController < ApplicationController
-
-  # def create
-  #   render text: params[:review].inspect
-  # end
+  before_filter :check
 
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.create(params[:review].permit(:rating, :description))
     @review.user = current_user
-    @review.save
-    redirect_to product_path(@product)
 
+    if @review.save
+      redirect_to product_path(@product)
+    end
+  end
+
+  def check
+    unless current_user
+      redirect_to product_path(@product)
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    redirect_to @review.product
   end
 end
